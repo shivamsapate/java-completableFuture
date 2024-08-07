de8cde10b83b582a34a850e11b26108080459f16
Shivam Sapate <shivam.sapate@gupshup.io>
2024-08-07 12:21:53 +0530

    initial commit

---
+++ src/main/java/com/shivam/java_async/JavaAsyncApplication.java
@@ -0,0 +1,13 @@
+package com.shivam.java_async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaAsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaAsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shivam/java_async/JavaAsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivam.java_async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
